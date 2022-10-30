d053462ea0768afa93c871d18da81891e1effa0e
vitaliksmile <vitaliksmile777@gmail.com>
2022-10-30 16:55:34 +0200

    done

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,17 @@
+const Filter = ({ handleChangeForm, value }) => {
+  return (
+    <input
+      placeholder="Find a contact"
+      onChange={handleChangeForm}
+      type="text"
+      name="filter"
+      value={value}
+    />
+  );
+};
+
+export default Filter;
+Filter.propTypes = {
+  handleChangeForm: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
